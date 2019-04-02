fcab7707f32e1c4c629eb1339e80cc58bde23ec8
hurk <huiskelvin@qq.com>
2019-04-02 15:42:56 +0800

    add User Center demo

---
+++ src/views/User.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="user">
+    <el-container>
+      <el-header class="header">
+        <span>logo</span>
+      </el-header>
+      <el-container>
+        <el-aside class="aside">
+          <el-menu :default-active="activeIndex" class="aside-menu" mode="vertical" @select="handleSelect">
+            <el-menu-item index="1"><span>问卷管理</span></el-menu-item>
+            <el-menu-item index="2"><span>个人信息</span></el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main class="main">main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: 1,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  background-color: aquamarine;
+}
+
+.aside {
+  width: 200px;
+  height: 100%;
+  background-color: aqua;
+}
+
+.main {
+  height: 100%;
+  width: calc(100% -200px);
+  background-color: blue;
+}
+</style>
