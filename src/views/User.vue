83d7cc960091efd095ad5264541095770a2e64c2
hurk <huiskelvin@qq.com>
2019-04-05 15:49:55 +0800

    update user center

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,13 +2,23 @@
   <div id="user">
     <el-container>
       <el-header class="header">
-        <span>logo</span>
+        <el-menu
+            :default-active="activeIndex"
+            class="header-nav"
+            @select="handleSelect"
+            mode="horizontal"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <el-menu-item index="1"><span>LOGO</span></el-menu-item>
+        </el-menu>
       </el-header>
       <el-container class="main">
         <el-aside class="aside">
           <el-menu
             :default-active="activeIndex"
-            class="aside-menu"
+            class="aside-nav"
             mode="vertical"
             @select="handleSelect"
           >
@@ -16,12 +26,12 @@
             <el-menu-item index="2">
               <router-link 
                 :to="{name: 'userInfo'}"
-                tag="span">个人信息</router-link>
+                tag="div">个人信息</router-link>
             </el-menu-item>
           </el-menu>
         </el-aside>
         <el-main class="main-content">
-          <router-view></router-view>
+          <router-view class="main-content-view"></router-view>
         </el-main>
       </el-container>
     </el-container>
@@ -38,28 +48,64 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+$box-shadow: 0 15px 30px rgba(0,0,0,0.1);
+
+.el-header {
+  padding: 0;
+}
+
 #user {
-  height: calc(100%);
+  height: 100vh;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
 }
 
 .header {
+  text-align: left;
   width: 100%;
   height: 40px;
   line-height: 40px;
+  .header-nav {
+    // width: 100%;
+  }
 }
 
 .main {
-  height: calc(100% - 40px);
+  width: 100%;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+
+  .aside {
+    width: 200px;
+    height: 100%;
+    .aside-nav {
+      height: 100%;
+    }
+  }
+
+  .main-content {
+    height: 100%;
+    background-color: #E5E9F2;
+    position: relative;
+    flex-grow: 1;
+
+    .main-content-view {
+      // min-height: calc(100vh - 200px);
+      background-color: #fff;
+      position: absolute;
+      top: 2em;
+      right: 1em;
+      bottom: 1em;
+      left: 1em;
+      box-shadow: $box-shadow;
+    }
+  }
+
 }
 
-.aside {
-  width: 200px;
-  height: 100%;
-}
 
-.main-content {
-  height: 100%;
-  width: calc(100% -200px);
-}
+
 </style>
