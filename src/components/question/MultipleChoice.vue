838ba37e464db8464c65c632ba8e9c53b39401df
tpwy <1979321568@qq.com>
2019-04-09 00:10:38 +0800

    改了一下变量名，添加isOperate的bool值操作编辑复制删除题型等功能的div元素

--- src/components/question/MultipleChoice.vue
+++ src/components/question/MultipleChoice.vue
@@ -8,10 +8,13 @@
         <el-radio-group v-model="select">
           <el-radio v-for="(choice, no) in choices" :key="no" v-bind:label="no">{{ choice }}</el-radio>
         </el-radio-group>
-        <el-button v-on:click="isEdit=true">编辑</el-button>
+        <div v-if="isOperate" class="edit_panel">
+        <!-- 编辑问卷的时候要用到的工具面板，但预览和发布后的问卷不需要显示 -->
+        <el-button v-on:click="editPanel=true">编辑</el-button>
+        </div>
       </div>
     </div>
-    <div v-if="isEdit" class="edit">
+    <div v-if="editPanel" class="edit">
       <div class="question_title edit">
         <el-input v-model="title" placeholder="请输入标题"></el-input>
       </div>
@@ -23,7 +26,7 @@
         <el-button v-on:click="removeChoice(no)" icon="el-icon-minus" circle></el-button>
         </div>
         <el-button v-on:click="addChoice()">添加选项</el-button>
-        <el-button v-on:click="isEdit=false">完成编辑</el-button>
+        <el-button v-on:click="editPanel=false">完成编辑</el-button>
         
       </div>
     </div>
@@ -38,7 +41,8 @@ export default {
       title: "标题",
       choices: ["选项1", "选项2"],
       select: null,
-      isEdit: true
+      isOperate: false,
+      editPanel: true
     };
   },
   methods: {
