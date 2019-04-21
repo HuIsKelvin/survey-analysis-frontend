11badd24b345f9b7220d1452a8e4fbd29a9922f2
hurk <huiskelvin@qq.com>
2019-04-21 23:50:17 +0800

    修改文件名大小写

---
+++ src/components/question/Introduction.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="question-edit-box" >
+    <el-input v-model="title"></el-input>
+    <el-input v-model="introduction" ></el-input>
+</div>
+</template>
+<script>
+import "@/assets/css/questionEdit.css"
+// import "@/assets/css/override-element-ui.css"
+
+export default {
+    data() {
+        return {
+            title:"问卷标题",
+            introduction:"感谢您能抽出几分钟时间来参加本次答题，现在我们就马上开始吧！"
+        }
+    }
+}
+</script>
+<style>
+/* .question-edit-box .el-input__inner {
+    border:0px;
+} */
+</style>
