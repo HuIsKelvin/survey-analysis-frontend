9178a194e016e023be00c2882048612c777d4186
hurk <huiskelvin@qq.com>
2019-04-21 23:27:39 +0800

    #

---
+++ src/components/questionaire/QusetionList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="question-list">
+    <div v-for="(qitem, index) in questionList" :key="index">{ qitem }</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuestionList",
+  data() {
+    return {
+      qusetionList: [1,2,3]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/questionaire/Manage.vue
+++ src/views/questionaire/Manage.vue
@@ -3,3 +3,12 @@
     
   </div>
 </template>
+
+<script>
+import QuestionList from "./../../components/questionaire/QusetionList"
+export default {
+  components: {
+    
+  }
+}
+</script>
