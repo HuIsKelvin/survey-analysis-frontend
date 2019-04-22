a2292e6d0e0062155f61f95c8ab94b70c0d42291
tpwy <1979321568@qq.com>
2019-04-23 00:01:39 +0800

    add: QuestionList添加按钮和日期显示，优化按钮右对齐和span间隔。添加About页

--- src/components/questionaire/QusetionList.vue
+++ src/components/questionaire/QusetionList.vue
@@ -4,33 +4,47 @@
       v-for="(qitem, index) in qusetionList"
       :key="index"
       class="box-card list-item"
-      shadow="hover">
+      shadow="hover"
+    >
       <div slot="header" class="list-item-header">
         <el-row>
           <el-col :span="12">
             <div class="q-main-info">
-              <span>id:<span>{{qitem.id}}</span></span>
-              <span>title:<span>{{qitem.title}}</span></span>
+              <span class="q-info">
+                id:
+                <span>{{qitem.id}}</span>
+              </span>
+              <span class="q-info">
+                title:
+                <span>{{qitem.title}}</span>
+              </span>
             </div>
           </el-col>
           <el-col :span="12">
             <div class="q-sub-info">
-              <span class="title">·<span class="status">运行中</span></span>
-              <span class="title">答卷:<span class="status">130</span></span>
+              <span class="title q-info">
+                ·
+                <span class="status">运行中</span>
+              </span>
+              <span class="title q-info">
+                答卷:
+                <span class="status">130</span>
+              </span>
+              <span class="title q-info">
+                发布日期:
+                <span class="status">4月20日 20:20</span>
+              </span>
             </div>
           </el-col>
         </el-row>
       </div>
-      <div>
-        <el-button
-          type="primary"
-          size="small">编辑</el-button>
-        <el-button
-          type="primary"
-          size="small">发布</el-button>
-        <el-button
-          type="primary"
-          size="small">停止</el-button>
+      <div class="q-controll">
+        <el-button class="controll-button" type="primary" size="small">编辑</el-button>
+        <el-button class="controll-button" type="primary" size="small">发布</el-button>
+        <el-button class="controll-button" type="primary" size="small">停止</el-button>
+        <el-button class="controll-button" type="primary" size="small">删除</el-button>
+        <el-button class="controll-button" type="primary" size="small">分析</el-button>
+        <el-button class="controll-button" type="primary" size="small">查看下载</el-button>
       </div>
     </el-card>
   </div>
@@ -51,9 +65,9 @@ export default {
           title: "第二"
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -64,12 +78,22 @@ export default {
     margin: 10px auto;
 
     .list-item-header {
+      .q-info {
+        margin: 10px;
+      }
+
       .q-main-info {
         // text-align: left;
       }
 
       .q-sub-info {
         text-align: right;
+      }
+    }
+    .q-controll {
+      float: right;
+      .controll-button {
+        float: left;
       }
     }
   }
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-card class="production-team">
+      <div slot="header" class="title">
+        <h1>制作团队</h1>
+      </div>
+      <div>
+        <el-collapse>
+          <el-collapse-item v-for="work in Object.keys(nameList)" :key="work" :name="work" :title="work">
+            <p v-for="name in nameList[work]" :key="name">{{ name }}</p>
+          </el-collapse-item>
+        </el-collapse>
+      </div>
+    </el-card>
   </div>
 </template>
+
+<script>
+export default {
+  name: "production-team",
+  data() {
+    return {
+      nameList: {
+        Frontend: ["胡瑞坤", "刘诗", "林键宇"],
+        Backend: ["陈硕", "廖健斌", "黄子希", "林键宇"],
+        Design: ["李佳纯", "林凯欣", "何钰怡", "魏依璐"]
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+span {
+  margin: 10px;
+}
+
+.production-team {
+  width: 500px;
+  margin: auto;
+}
+</style>
+
