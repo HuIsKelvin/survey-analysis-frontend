31c3c97da938bf5d2a437885380d34abd7b704e7
tpwy <1979321568@qq.com>
2019-06-10 23:16:41 +0800

    add: 添加可视化页面：Report为父页面，Analysis为分析页面，Answer为答案预览和下载页面

---
+++ src/views/questionaire/Report.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="user">
+    <el-container>
+      <el-header class="header">
+        <el-menu
+          :default-active="activeIndex"
+          class="header-nav"
+          mode="horizontal"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-menu-item index="1">
+            <router-link :to="{name: 'report.analysis'}" tag="div">统计分析</router-link>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <router-link :to="{name: 'report.answer'}" tag="div">查看答卷</router-link>
+          </el-menu-item>
+          <!-- 头像 -->
+        </el-menu>
+      </el-header>
+      <el-main class="main-content">
+        <router-view class="main-content-view"></router-view>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+
+<script>
+export default {
+    name: "Report",
+    data() {
+        return {
+            activeIndex: "1",
+        }
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
