91fe9c1c104b0de346a9712e522b14ef136f4d9a
tpwy <1979321568@qq.com>
2019-06-17 00:58:03 +0800

    add: 管理页面添加下载答卷和分析按钮功能

--- src/components/questionaire/QusetionList.vue
+++ src/components/questionaire/QusetionList.vue
@@ -10,13 +10,11 @@
         <el-row>
           <el-col :span="12">
             <div class="q-main-info">
-              <span class="q-info">
-                {{ qindex + 1 }}
-              </span>
+              <span class="q-info">{{ qindex + 1 }}</span>
               <!-- <span class="q-info">
                 ID:
                 <span>{{qitem.id}}</span>
-              </span> -->
+              </span>-->
               <span class="q-info">
                 <!-- 问卷名: -->
                 <span>{{qitem.name}}</span>
@@ -28,12 +26,8 @@
               <span class="title q-info">
                 ·
                 <!-- 运行状态:  -->
-                <span v-if="qitem.state === true" class="status">
-                  运行中
-                </span>
-                <span v-else class="status">
-                  暂停中
-                </span>
+                <span v-if="qitem.state === true" class="status">运行中</span>
+                <span v-else class="status">暂停中</span>
               </span>
               <span class="title q-info">
                 答卷数量:
@@ -48,14 +42,9 @@
           </el-col>
         </el-row>
       </div>
-      <el-row 
-        type="flex"
-        justify="end"
-        class="q-control"
-      >
-
+      <el-row type="flex" justify="end" class="q-control">
         <!-- 编辑-按钮 -->
-        <el-button 
+        <el-button
           class="control-button"
           type="primary"
           plain
@@ -69,7 +58,9 @@
         <!-- 发布 或 停止-按钮 -->
         <el-button
           class="control-button"
-          type="primary" plain size="small"
+          type="primary"
+          plain
+          size="small"
           @click="changeQuestionnaireState(qitem.id, true)"
         >
           <i class="el-icon-check"></i>
@@ -77,7 +68,9 @@
         </el-button>
         <el-button
           class="control-button"
-          type="primary" plain size="small"
+          type="primary"
+          plain
+          size="small"
           @click="changeQuestionnaireState(qitem.id, false)"
         >
           <i class="el-icon-close"></i>
@@ -85,37 +78,32 @@
         </el-button>
 
         <!-- 分析-按钮 -->
-        <el-button class="control-button" type="primary" plain size="small">
+        <el-button class="control-button" type="primary" plain size="small" @click="$router.push({path:'/report/'+qitem.id})">
           <i class="el-icon-document"></i>
           分析
         </el-button>
 
         <!-- 下载-按钮 -->
-        <el-button class="control-button" type="primary" plain size="small">
+        <el-button class="control-button" type="primary" plain size="small" @click="download_excel(qitem.id)">
           <i class="el-icon-download"></i>
-          查看下载
+          下载答卷
         </el-button>
 
         <!-- 分享-按钮 -->
-        <el-popover
-          placement="bottom"
-          width="250"
-          trigger="hover"
-        >
+        <el-popover placement="bottom" width="250" trigger="hover">
           <!-- popover content -->
           <div class="popover-content">
             <p>链接：</p>
-            <el-input 
-              readonly="readonly"
-              :value="shareBaseUrl + qitem.id"
-            ></el-input>
+            <el-input readonly="readonly" :value="shareBaseUrl + qitem.id"></el-input>
             <p>二维码</p>
             <qrcode :value="shareBaseUrl + qitem.id" :options="{ width: 150 }"></qrcode>
           </div>
           <!-- popover trigger -->
-          <el-button 
+          <el-button
             class="control-button button-share"
-            type="primary" plain size="small"
+            type="primary"
+            plain
+            size="small"
             slot="reference"
           >
             <i class="el-icon-share"></i>
@@ -126,7 +114,9 @@
         <!-- 删除-按钮 -->
         <el-button
           class="control-button"
-          type="danger" plain size="small"
+          type="danger"
+          plain
+          size="small"
           @click="deleteQuestionnaire(qitem.id)"
         >
           <i class="el-icon-delete"></i>
@@ -153,17 +143,22 @@ export default {
   computed: {
     shareBaseUrl() {
       const baseUrl = document.location.origin;
-      return (baseUrl + "/survey/");
-    }
+      return baseUrl + "/survey/";
+    },
+    
   },
-  methods : {
+  methods: {
     ...mapMutations({
       set_uQL: "set_userQuestionList"
     }),
 
     // 获得当前 user 所拥有的问卷
     getQuestionnaires() {
-      axios.get("/questionnaires/search/userId?userId="+this.$store.state.user.userId)
+      axios
+        .get(
+          "/questionnaires/search/userId?userId=" +
+            this.$store.state.user.userId
+        )
         .then(response => {
           // TODO: 对数据进行处理已获得更好的展示效果
           this.questionnaires = response.data._embedded.questionnaires;
@@ -171,17 +166,45 @@ export default {
         })
         .catch(err => {
           console.log(err);
-        })
+        });
     },
 
+    download_excel(qid) {
+      console.log(qid)
+      let options = {
+        responseType: "blob"
+      };
+      axiosVisual
+        .get("excel/" + qid, options)
+        .then(response => {
+          const content = response.data;
+          const blob = new Blob([content]);
+          const filename = `${qid}.xlsx`;
+          const elink = document.createElement("a");
+          elink.download = filename;
+          //elink.style.display = "none";
+          elink.href = URL.createObjectURL(blob);
+          document.body.appendChild(elink);
+          elink.click();
+          URL.revokeObjectURL(elink);
+          document.body.removeChild(elink);
+        })
+        .catch(error => {
+          this.$message.error(error.response.data.error);
+        });
+    },
     // 编辑问卷
     editQuestionnaire(qid, qindex) {
       // this.$router.push({ name: "questionEdit", params:{ qid: qid } });
-      this.$confirm("已发布的问卷再次编辑后，会丢失以前的答卷数据。是否确认编辑？", "提示", { type: "warning" })
+      this.$confirm(
+        "已发布的问卷再次编辑后，会丢失以前的答卷数据。是否确认编辑？",
+        "提示",
+        { type: "warning" }
+      )
         .then(res => {
           this.set_uQL(this.questionnaires[qindex]);
           console.log(this.questionnaires[qindex]);
-          this.$router.push({ name: "questionEdit", params:{ qid: qid } });
+          this.$router.push({ name: "questionEdit", params: { qid: qid } });
           console.log(qid);
         })
         .catch(err => {});
@@ -189,19 +212,20 @@ export default {
 
     // 发布问卷
     releaseQuestionnaire(qid) {
-      axios.patch("/questionnaires/" + qid, { state: true })
+      axios.patch("/questionnaires/" + qid, { state: true });
     },
 
     // 修改问卷状态
     // qid：问卷id；
     // qstate：发布状态；true为发布中，false为暂停中
     changeQuestionnaireState(qid, qState) {
-      axios.patch("/questionnaires/" + qid, { state: qState })
+      axios
+        .patch("/questionnaires/" + qid, { state: qState })
         .then(res => {
           console.log("change questionnaire state! " + qState);
           this.getQuestionnaires();
         })
-        .catch( err => console.log(err) );
+        .catch(err => console.log(err));
     },
 
     // 删除问卷
@@ -210,24 +234,26 @@ export default {
         confirmButtonText: "确定",
         cancelButtonText: "取消",
         type: "warning"
-      }).then(() => {
-        axios.delete("/questionnaires/" + qid)
-          .then(res => {
-            console.log("delete: " + qid);
-            // 更新页面问卷列表
-            this.getQuestionnaires();
-            this.$message({ type: "success", message: "删除成功!" });
-          })
-          .catch(err => {
-            console.log(err);
-            this.$message({ type: "error", message: "删除出现错误！" });
-          })
-      }).catch(() => {
-        this.$message({ type: "info", message: "已取消删除" });          
-      });
+      })
+        .then(() => {
+          axios
+            .delete("/questionnaires/" + qid)
+            .then(res => {
+              console.log("delete: " + qid);
+              // 更新页面问卷列表
+              this.getQuestionnaires();
+              this.$message({ type: "success", message: "删除成功!" });
+            })
+            .catch(err => {
+              console.log(err);
+              this.$message({ type: "error", message: "删除出现错误！" });
+            });
+        })
+        .catch(() => {
+          this.$message({ type: "info", message: "已取消删除" });
+        });
     }
   }
-
 };
 </script>
 
