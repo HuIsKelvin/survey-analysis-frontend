ebba1585729496d90dd86e67e31e65cc94a03b30
hurk <huiskelvin@qq.com>
2019-06-17 15:14:23 +0800

    add: 新增找回密码功能

--- src/components/sign/FindPwdForm.vue
+++ src/components/sign/FindPwdForm.vue
@@ -8,13 +8,14 @@
       <el-form-item label="电子邮件地址" type="email" prop="email">
         <el-input v-model="findPwdForm.email"></el-input>
       </el-form-item>
-      <el-form-item label="新密码" type="password" prop="password">
-        <el-input v-model="findPwdForm.password"></el-input>
+      <el-form-item label="新密码" type="password" prop="newPassword">
+        <el-input type="password" v-model="findPwdForm.newPassword"></el-input>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm('findPwdForm')">提交</el-button>
       </el-form-item>
     </el-form>
+    <p>返回登录？ <router-link :to="{name: 'sign'}" tag="span" class="sign-container-switch">登录</router-link></p>
   </div>
 </template>
 
@@ -22,6 +23,21 @@
 export default {
   name: "FindPwdForm",
   data() {
+    var validPass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        var re = /^(?![A-Z]+$)(?![a-z]+$)(?!\d+$)(?![\W_]+$)\S{6,32}$/;
+        if (!re.test(value)) {
+          callback(new Error("密码不能为纯数学或纯字母"));
+        } else {
+          if (this.findPwdForm.newPassword !== "") {
+            this.$refs.findPwdForm.validateField("checkPass");
+          }
+        }
+        callback();
+      }
+    };
     var validEmail = (rule, value, callback) => {
       if (value === "") {
         callback(new Error("电子邮件地址不能为空"));
@@ -37,18 +53,29 @@ export default {
     return {
       findPwdForm: {
         name: "",
-        email: ""
+        email: "",
+        newPassword: ""
       },
+      // TODO: 增加密码的校验规则
       rules: {
         name: [
-          { required: true, message: "请输入用户名", trigger: "blur" }
+          { required: true, message: "请输入用户名", trigger: "change" }
         ],
         email: [
           {
             required: true,
             validator: validEmail,
-            trigger: "blur"
+            trigger: "change"
           }
+        ],
+        newPassword: [
+          {
+            min: 6,
+            max: 32,
+            message: "密码长度需为6-32个字符",
+            trigger: "blur"
+          },
+          { required: true, validator: validPass, trigger: "change" }
         ],
       }
     }
@@ -57,7 +84,20 @@ export default {
     submitForm(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
-          alert("submit!");
+          let { name, email, newPassword } = this.findPwdForm;
+          console.log(name);
+          console.log(email);
+          console.log(newPassword);
+          axios.post("/auth/forget/password?email=" + email + "&uname=" + name, { password: newPassword })
+            .then(res => {
+              console.log("find pwd success!");
+              console.log(res);
+              this.$message({ message: "找回密码成功！", type: "success" });
+            })
+            .then(res => {
+              this.$message({ message: "请返回登录！", type: "info" });
+            })
+            .catch(err => { console.log(err); });
         } else {
           console.log("error submit!");
           return false;
@@ -69,5 +109,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+#find-pwd-form {
+  .sign-container-switch {
+    cursor: pointer;
+    color: #41b883;
+  }
+}
 </style>
