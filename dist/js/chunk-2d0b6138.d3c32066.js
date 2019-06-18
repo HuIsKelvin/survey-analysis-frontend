4de6bcf4bfeb163908202f43c2838f59c9878c37
hurk <huiskelvin@qq.com>
2019-06-18 18:29:58 +0800

    add: build the project

---
+++ dist/js/chunk-2d0b6138.d3c32066.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b6138"],{"1c2b":function(t,n,e){"use strict";e.r(n);var r=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",[e("charts",{attrs:{options:JSON.parse(t.report.data.bar)}})],1)},a=[],c=e("7ea1"),o={props:{report:null},mounted:function(){},components:{charts:c["a"]}},s=o,p=e("17cc"),u=Object(p["a"])(s,r,a,!1,null,"2fca6b84",null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0b6138.d3c32066.js.map
